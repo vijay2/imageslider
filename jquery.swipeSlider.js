66cbfc44bf8847fddd32958174dcf4995a79a016
vijay2 <vijay7002@gmail.com>
2016-11-27 23:08:53 +0530

    updated files

---
+++ jquery.swipeSlider.js
@@ -0,0 +1,34 @@
+(function($) {
+    $.fn.swipeSlider = function() {
+        var element = this;
+        var imgEle = element.find('img');
+        var txtEle = element.find("#slider");
+        var txtwidth;
+
+        $(imgEle).load(function() {
+            txtwidth = txtEle.width();
+            var imageWidth = imgEle.width();
+            var imageHeight = imgEle.height();
+            element.css('width', imageWidth);
+            element.find('.after').css('width', imageWidth / 2);
+            txtEle.css('left', imageWidth / 2 - txtwidth / 2 + "px");
+            element.bind('touchmove mousemove', function(e) {
+                txtEle.css('display', 'none');
+                var currentY = e.originalEvent.touches ? e.originalEvent.touches[0].pageY : e.pageY;
+                var currentX = e.originalEvent.touches ? e.originalEvent.touches[0].pageX : e.pageX;
+                var x = currentX - this.offsetLeft;
+                var y = currentY - this.offsetTop;
+                element.find(".after").css("width", x);
+                //   console.log(x)
+                if (x >= 10 && x <= imageWidth - 10) {
+                    element.find('.after').addClass("addborder");
+                } else {
+                    element.find('.after').removeClass("addborder");
+                    element.find('.after').css("border", "none");
+                }
+            });
+        });
+
+
+    };
+})(jQuery);
